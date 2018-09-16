2ff2885e6c7af249539ff575e39bbfb1ccf57255
Funzo1404 <damien.boulanger2@sfr.fr>
2018-09-16 12:57:03 +0200

    Update FontEngine.js

--- FontEngine.js
+++ FontEngine.js
@@ -69,10 +69,12 @@ Sburb.FontEngine.prototype.prefixColours = {
 	rose:"#b536da",aranea : "#005682",
 	kankri:"#ff0000",porrim: "#008141",
 	latula:"#008282",season:"#00fa9a",
-	asceli:"#000000",arseck: "#d26912",
+	asceli:"#000000",akuuma:"#cc0000",
+	arseck: "#d26912",
 	ruvvik: "#d26912",seferi: "#b69c00",
 	sophia:"#b68c00",eliseo:"#658200",
 	lysdia: "#008141",gardia: "#004121",
+	asarin:"#00cc66",
 	arkeah:"#005682",dultew:"#005682",
 	viveil:"#000056",sollas:"#000056",
 	ahvili:"#658200",dirk:"#f2a400"
